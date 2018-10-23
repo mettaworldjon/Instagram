77fbd01c00ba76f49e0b6c491f636c88429b2a03
mettaworldj <jonathan@myiostech.com>
2018-10-22 23:32:05 -0500

    Image Upload

--- Instagram/SignUp/AddPhoto+Register/AddPhotoController.swift
+++ Instagram/SignUp/AddPhoto+Register/AddPhotoController.swift
@@ -20,6 +20,56 @@ class AddPhotoController: RegisterController {
     
     @objc func handleSignUp() {
         
+        guard let email = self.registerData?.getEmail() else { return }
+        guard let username = self.registerData?.getName() else { return }
+        guard let password = self.registerData?.getPassword() else { return }
+        
+        // Create User
+        Auth.auth().createUser(withEmail: email, password: password) { (user, err:Error?) in
+            // Check for Error
+            if let error = err {
+                print("Failed to create user:", error)
+                return // Kill
+            }
+            print("Successfully created user!")
+            
+            // Prepare Image
+            guard let image = self.ui.addImageBtn.imageView?.image else { return }
+            guard let uploadData = image.jpegData(compressionQuality: 0.3) else { return }
+            let filename = NSUUID().uuidString
+            
+            // Upload Image
+            let storageRef = Storage.storage().reference().child("profile_images").child(filename)
+            storageRef.putData(uploadData, metadata: nil, completion: { (metadata, err) in
+                
+                // Check for Error
+                if let error = err {
+                    print("Failed to upload profile image:",error)
+                    return
+                }
+                
+                // Get Image URL
+                storageRef.downloadURL(completion: { (downloadURL, err) in
+                    
+                    // Check for Error
+                    if let error = err {
+                        print("Failed to get URL for image:", error)
+                        return
+                    }
+                    
+                    // Obtained URL
+                    guard let profileImageUrl = downloadURL?.absoluteString else { return }
+                    print("Successfully uploaded profile image: ", profileImageUrl)
+                    
+                })
+                
+            })
+            
+            
+            
+            
+        }
+        
     }
     
 }
